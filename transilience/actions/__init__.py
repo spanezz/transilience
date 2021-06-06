819d2d8f7381c5b831f1c8b6eff5cbf77680acf2
Enrico Zini <enrico@enricozini.org>
2021-06-06 15:35:27 +0200

    Import facts in actions

--- transilience/actions/__init__.py
+++ transilience/actions/__init__.py
@@ -5,5 +5,6 @@ from .blockinfile import BlockInFile
 from .apt import Apt
 from .command import Command
 from .systemd import Systemd
+from . import facts
 
-__all__ = ["Action", "File", "Copy", "BlockInFile", "Apt", "Command", "Systemd"]
+__all__ = ["Action", "File", "Copy", "BlockInFile", "Apt", "Command", "Systemd", "facts"]
