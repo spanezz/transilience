a1307224976c2db58570650aa0bbdd0ac053d95c
Enrico Zini <enrico@enricozini.org>
2021-06-13 13:43:17 +0200

    Added Fail action

--- transilience/actions/misc.py
+++ transilience/actions/misc.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
+from typing import TYPE_CHECKING
 from dataclasses import dataclass
 from .action import Action
 from . import builtin
+
+if TYPE_CHECKING:
+    import transilience.system
 
 
 @builtin.action(name="noop")
@@ -10,5 +14,28 @@ class Noop(Action):
     """
     Do nothing, successfully
     """
+    changed: bool = False
+
     def summary(self):
         return "Do nothing"
+
+    def run(self, system: transilience.system.System):
+        super().run(system)
+        if self.changed:
+            self.set_changed()
+
+
+@builtin.action(name="fail")
+@dataclass
+class Fail(Action):
+    """
+    Fail with a custom message
+    """
+    msg: str = "Failed as requested from task"
+
+    def summary(self):
+        return f"Fail: {self.msg}"
+
+    def run(self, system: transilience.system.System):
+        super().run(system)
+        raise RuntimeError(self.msg)
