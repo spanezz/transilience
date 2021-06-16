550f9521a4fc3fee76f11106852b538b508f92bd
Denis Laxalde <denis.laxalde@dalibo.com>
2021-06-16 09:25:07 +0200

    Bootstrap Python packaging
    We add a setup.py with basic metadata and dependencies. Only "jinja2"
    and "mitogen" dependencies are required. Other ones appear to be
    optional: "import coloredlogs" is handled, and "parted" is only required
    for the "device" module which is apparently not imported internally (the
    dependency is thus defined as an "extra").
    
    A MANIFEST.in file is also added to configure sdist. We exclude
    developer scripts, probably not useful to distribute as is.

---
+++ setup.py
@@ -0,0 +1,39 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="transilience",
+    version="0.1.0.dev0",
+    description="A provisioning library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spanezz/transilience/",
+    author="Enrico Zini",
+    author_email="enrico@enricozini.org",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Systems Administration",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    packages=find_packages(where="."),
+    python_requires=">=3.7, <4",
+    install_requires=[
+        # "coloredlogs", # Optional
+        "jinja2",
+        "mitogen",
+    ],
+    extras_require={
+        "device": ["parted"],
+    },
+)
