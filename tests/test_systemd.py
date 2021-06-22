3c80563e1d0cd80ab13dc6c08fa3c46d6555d6a8
Enrico Zini <enrico@enricozini.org>
2021-06-22 22:42:11 +0200

    Implemented check mode for systemd

--- tests/test_systemd.py
+++ tests/test_systemd.py
@@ -1,12 +1,124 @@
 from __future__ import annotations
+from typing import Optional, List, Dict
 import unittest
+from unittest import mock
 import inspect
+import shlex
 import uuid
-from transilience.unittest import ActionTestMixin, ChrootTestMixin
+from transilience.unittest import ActionTestMixin, LocalTestMixin, ChrootTestMixin
 from transilience.actions import builtin
 
 
-class TestSystemd(ActionTestMixin, ChrootTestMixin, unittest.TestCase):
+class TestSystemd(ActionTestMixin, LocalTestMixin, unittest.TestCase):
+    def assertSystemd(
+                self,
+                file_state: Optional[str] = None,
+                active_state: Optional[str] = None,
+                called: Optional[List[str]] = None,
+                changed=True,
+                **kwargs):
+        unit_info: Dict[str, str] = {}
+        if file_state is not None:
+            unit_info["UnitFileState"] = file_state
+        if active_state is not None:
+            unit_info["ActiveState"] = active_state
+        if called is None:
+            called = []
+        actual_called = []
+
+        def collect(args, **kw):
+            self.assertIn("systemctl", args[0])
+            actual_called.append(" ".join(shlex.quote(a) for a in args[1:]))
+
+        with mock.patch("transilience.actions.systemd.Systemd.get_unit_info", return_value=unit_info):
+            # Try check mode first
+            with mock.patch("subprocess.run", collect):
+                orig = builtin.systemd(check=True, **kwargs)
+                self.run_action(orig, changed=changed)
+                self.assertEqual(actual_called, [])
+
+            with mock.patch("subprocess.run", collect):
+                orig = builtin.systemd(**kwargs)
+                return self.run_action(orig, changed=changed), actual_called
+
+    def test_daemon_reload(self):
+        act, called = self.assertSystemd(daemon_reload=True, changed=False)
+        self.assertEqual(called, ["daemon-reload"])
+
+    def test_daemon_reexec(self):
+        act, called = self.assertSystemd(daemon_reexec=True, changed=False)
+        self.assertEqual(called, ["daemon-reexec"])
+
+    def test_enable(self):
+        act, called = self.assertSystemd(
+                unit="test", file_state="disabled", active_state="inactive", enabled=False, changed=False)
+        self.assertEqual(called, [])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="disabled", active_state="inactive", enabled=True, changed=True)
+        self.assertEqual(called, ["enable test"])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="inactive", enabled=True, changed=False)
+        self.assertEqual(called, [])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="inactive", enabled=False, changed=True)
+        self.assertEqual(called, ["disable test"])
+
+    def test_mask(self):
+        act, called = self.assertSystemd(
+                unit="test", file_state="disabled", active_state="inactive", masked=False, changed=False)
+        self.assertEqual(called, [])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="disabled", active_state="inactive", masked=True, changed=True)
+        self.assertEqual(called, ["mask test"])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="masked", active_state="inactive", masked=True, changed=False)
+        self.assertEqual(called, [])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="masked", active_state="inactive", masked=False, changed=True)
+        self.assertEqual(called, ["unmask test"])
+
+    def test_start(self):
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="inactive", state="stopped", changed=False)
+        self.assertEqual(called, [])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="inactive", state="started", changed=True)
+        self.assertEqual(called, ["start test"])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="active", state="started", changed=False)
+        self.assertEqual(called, [])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="active", state="stopped", changed=True)
+        self.assertEqual(called, ["stop test"])
+
+    def test_reload(self):
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="active", state="restarted", changed=True)
+        self.assertEqual(called, ["restart test"])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="inactive", state="restarted", changed=True)
+        self.assertEqual(called, ["start test"])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="active", state="reloaded", changed=True)
+        self.assertEqual(called, ["reload test"])
+
+        act, called = self.assertSystemd(
+                unit="test", file_state="enabled", active_state="inactive", state="reloaded", changed=True)
+        self.assertEqual(called, ["start test"])
+
+
+class TestSystemdReal(ActionTestMixin, ChrootTestMixin, unittest.TestCase):
     def assertSystemd(self, changed=True, **kwargs):
         orig = builtin.systemd(**kwargs)
         return self.run_action(orig, changed=changed)
--- transilience/actions/systemd.py
+++ transilience/actions/systemd.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, Dict
 from dataclasses import dataclass
 import subprocess
 import shutil
@@ -71,65 +71,77 @@ class Systemd(Action):
 
         return summary
 
+    def run_systemctl(self, *args, allow_check=False, **kw):
+        """
+        Run systemctl with logging and common subprocess args
+
+        If allow_check is True, run the command also in check mode
+        """
+        kw.setdefault("env", self._default_env)
+        kw.setdefault("check", True)
+        kw.setdefault("capture_output", True)
+        systemctl_cmd = self._default_systemctl_cmd + list(args)
+        formatted_cmd = " ".join(shlex.quote(x) for x in systemctl_cmd)
+        self.log.info("running %s", formatted_cmd)
+        if self.check and not allow_check:
+            return
+        try:
+            return subprocess.run(systemctl_cmd, **kw)
+        except subprocess.CalledProcessError as e:
+            self.log.error("%s: exited with code %d and stderr %r", formatted_cmd, e.returncode, e.stderr)
+            raise
+
+    def get_unit_info(self) -> Dict[str. str]:
+        """
+        Fetch the current status of the unit
+
+        Documentation of UnitFileState values can be found in man systemctl(1)
+        """
+        res = self.run_systemctl("show", self.unit, "--no-page", check=False, text=True, allow_check=True)
+
+        unit_info: Dict[str, str] = {}
+        if res.returncode == 0:
+            for line in res.stdout.splitlines():
+                k, v = line.strip().split("=", 1)
+                unit_info[k] = v
+
+        return unit_info
+
     def run(self, system: transilience.system.System):
         super().run(system)
+        self._default_env = dict(os.environ)
+        if "XDG_RUNTIME_DIR" not in self._default_env:
+            self._default_env["XDG_RUNTIME_DIR"] = f"/run/user/{os.geteuid()}"
 
         systemctl = shutil.which("systemctl")
         if systemctl is None:
             raise RuntimeError("systemctl not found")
 
-        cmd = [systemctl]
+        self._default_systemctl_cmd = [systemctl]
 
         if self.scope != "system":
-            cmd.append(f"--{self.scope}")
+            self._default_systemctl_cmd.append(f"--{self.scope}")
 
         if self.no_block:
-            cmd.append("--no-block")
+            self._default_systemctl_cmd.append("--no-block")
 
         if self.force:
-            cmd.append("--force")
-
-        env = dict(os.environ)
-        if "XDG_RUNTIME_DIR" not in env:
-            env["XDG_RUNTIME_DIR"] = f"/run/user/{os.geteuid()}"
-
-        def run_systemctl(*args, **kw):
-            """
-            Run systemctl with logging and common subprocess args
-            """
-            kw.setdefault("env", env)
-            kw.setdefault("check", True)
-            kw.setdefault("capture_output", True)
-            systemctl_cmd = cmd + list(args)
-            formatted_cmd = " ".join(shlex.quote(x) for x in systemctl_cmd)
-            self.log.info("running %s", formatted_cmd)
-            try:
-                return subprocess.run(systemctl_cmd, **kw)
-            except subprocess.CalledProcessError as e:
-                self.log.error("%s: exited with code %d and stderr %r", formatted_cmd, e.returncode, e.stderr)
-                raise
+            self._default_systemctl_cmd.append("--force")
 
         if self.daemon_reload:
-            run_systemctl("daemon-reload")
+            self.run_systemctl("daemon-reload")
 
         if self.daemon_reexec:
-            run_systemctl("daemon-reexec")
+            self.run_systemctl("daemon-reexec")
 
         if self.unit is not None:
-            # Fetch the current status of the unit
             # Documentation of UnitFileState values can be found in man systemctl(1)
-            res = run_systemctl("show", self.unit, "--no-page", check=False, text=True)
-
-            unit_info = {}
-            if res.returncode == 0:
-                for line in res.stdout.splitlines():
-                    k, v = line.strip().split("=", 1)
-                    unit_info[k] = v
+            unit_info = self.get_unit_info()
 
             if self.masked is not None:
                 orig_masked = unit_info.get("UnitFileState") == "masked"
                 if self.masked != orig_masked:
-                    run_systemctl("mask" if self.masked else "unmask", self.unit)
+                    self.run_systemctl("mask" if self.masked else "unmask", self.unit)
                     self.set_changed()
 
             if self.enabled is not None:
@@ -137,7 +149,7 @@ class Systemd(Action):
                         "enabled", "enabled-runtime", "alias", "static",
                         "indirect", "generated", "transient")
                 if self.enabled != orig_enabled:
-                    run_systemctl("enable" if self.enabled else "disable", self.unit)
+                    self.run_systemctl("enable" if self.enabled else "disable", self.unit)
                     self.set_changed()
 
             if self.state is not None:
@@ -163,5 +175,5 @@ class Systemd(Action):
                             action = "restart"
 
                 if action is not None:
-                    run_systemctl(action, self.unit)
+                    self.run_systemctl(action, self.unit)
                     self.set_changed()
